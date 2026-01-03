78900d4350ab98e8a5ceca6a2d0cacbb1a50a390
Unbothered Dev <40361000+jerryisuwamakeri@users.noreply.github.com>
2026-01-03 01:14:56 +0100

    Aggressive responsive fixes for iPhone SE - reduce text sizes, fix menu/footer overflow

--- components/menuLinks.vue
+++ components/menuLinks.vue
@@ -1,21 +1,21 @@
 <template>
-	<ul>
-	  <li class="inline-block mx-2 font-bold text-gray-600 text-xs uppercase hover:text-gray-800">
+	<ul class="flex flex-nowrap gap-1 sm:gap-3 px-2">
+	  <li class="font-bold text-gray-600 text-xs uppercase hover:text-gray-800 whitespace-nowrap">
 	  	<nuxt-link :to="{'name': 'index'}">
 	  		Home
 	  	</nuxt-link>
 	  </li>
-	  <li class="inline-block mx-2 font-bold text-gray-600 text-xs uppercase hover:text-gray-800">
+	  <li class="font-bold text-gray-600 text-xs uppercase hover:text-gray-800 whitespace-nowrap">
 	  	<nuxt-link :to="{'name': 'about'}">
 	  		About
 	  	</nuxt-link>
 	  </li>
-	  <li class="inline-block mx-2 font-bold text-gray-600 text-xs uppercase hover:text-gray-800">
+	  <li class="font-bold text-gray-600 text-xs uppercase hover:text-gray-800 whitespace-nowrap">
 	  	<nuxt-link :to="{'name': 'work'}">
 	  		Work
 	  	</nuxt-link>
 	  </li>
-	  <li class="inline-block mx-2 font-bold text-gray-600 text-xs uppercase hover:text-gray-800">
+	  <li class="font-bold text-gray-600 text-xs uppercase hover:text-gray-800 whitespace-nowrap">
 	  	<nuxt-link :to="{'name': 'contact'}">
 	  		Contact
 	  	</nuxt-link>
