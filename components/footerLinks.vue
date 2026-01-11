3ea13daf0840b7f7013f881e3ac13a097e2a1cef
Makeri J.I <40361000+jerryisuwamakeri@users.noreply.github.com>
2026-01-11 17:20:17 +0100

    Update email address in footer links

--- components/footerLinks.vue
+++ components/footerLinks.vue
@@ -17,7 +17,7 @@
 			  </a>
 			</li>
 			<li class="font-bold flex-shrink-0">
-			  <a href="mailto:holla@jerryisuwamakeri.com?subject=Hi, Jerry">
+			  <a href="mailto:makerijerry.dev@gmail.com?subject=Hi, Jerry">
 			    <svg class="h-5 w-5 sm:h-6 sm:w-6 text-gray-600" fill="currentColor" viewBox="0 0 20 20"><path d="M18 2a2 2 0 0 1 2 2v12a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V4c0-1.1.9-2 2-2h16zm-4.37 9.1L20 16v-2l-5.12-3.9L20 6V4l-10 8L0 4v2l5.12 4.1L0 14v2l6.37-4.9L10 14l3.63-2.9z"/></svg>
 			  </a>
 			</li>
